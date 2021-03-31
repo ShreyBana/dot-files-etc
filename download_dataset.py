17999ba3c329bc04ee645ac0baf0cb5e53669910
ShreyBana <shreybana26@gmail.com>
2021-03-31 19:52:22 +0530

    updated dataset download scirpt

--- download_dataset.py
+++ download_dataset.py
@@ -1,31 +1,35 @@
-import sys
 import requests
+import sys
 import os
 
-url = sys.argv[1]
-save_path = sys.argv[2]
+DATASETS_PATH = '/home/shrey_bana/projects/datasets/'
 MB = 1024 * 1024
-
-r = requests.get(url, stream=True)
-size = int(r.headers.get('content-length')) / MB
-
-if os.path.exists(save_path) and os.stat(save_path).st_size == size * MB:
-    print('File already exists')
-    exit()
-
-print('Downloading {}'.format(save_path.split('/')[-1]))
-print('Total Size: {:.2f} MB'.format(size))
 
 def print_progress(percentage):
     print('%{:.2f} '.format(percentage) + '[' + '=' * int(percentage / 10) + ' ' * (10 - int(percentage / 10)) + ']', end='\r')
 
-with open(save_path, 'wb') as fd:
-    downloaded = 0
-    print_progress(downloaded * 100 / size)
-    for chunk in r.iter_content(chunk_size=int(size * 0.1) * MB): # set to 10% of total size of the file
-        fd.write(chunk)
-        downloaded += 0.1 * size
-        downloaded = min(downloaded, size)
-        print_progress(downloaded * 100 / size)
+for url in sys.argv[1:]:
+    print('---------------------------------')
+    filename = url.split('/')[-1]
+    path = DATASETS_PATH + filename
 
-print('\nDownload Complete')
+    r = requests.get(url, stream=True)
+    size = int(r.headers.get('content-length')) / MB
+
+    if os.path.exists(path) and os.stat(path).st_size == size * MB:
+        print('{} already exists'.format(filename))
+        continue
+
+    print('Downloading {}'.format(filename))
+    print('Total Size: {:.2f} MB'.format(size))
+    chunk_size = min(int(size * 0.1) * MB, int(MB)) # set to 10% of total size of the file but lower bounded by 1 MB
+    with open(path, 'wb') as fd:
+        downloaded = 0
+        print_progress(downloaded * 100 / size)
+        for chunk in r.iter_content(chunk_size=int(size * 0.1) * MB): # set to 10% of total size of the file
+            fd.write(chunk)
+            downloaded += 0.1 * size
+            downloaded = min(downloaded, size)
+            print_progress(downloaded * 100 / size)
+
+    print('\n')
