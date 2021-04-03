0d1294b9f00335dc87021f404d43b149b170bae3
ShreyBana <shreybana26@gmail.com>
2021-04-03 14:49:18 +0530

    updated script

--- download_dataset.py
+++ download_dataset.py
@@ -3,9 +3,9 @@ import requests
 import sys
 import os
 
-DATASETS_PATH = ''
+DATASETS_PATH = '' # edit in your download path
 MB = 1024 * 1024
-URL_LIST = []
+URL_LIST = sys.argv[1:]
 
 def print_progress(percentage):
     print('\r%{:.2f} '.format(percentage) + '[' + '=' * int(percentage * 3 / 10) + ' ' * (30 - int(percentage * 3 / 10)) + ']', end='\0')
@@ -18,8 +18,8 @@ for url in URL_LIST:
     r = requests.get(url, stream=True)
     size = int(r.headers.get('content-length')) / MB
 
-    if os.path.exists(path) and os.stat(path).st_size == size * MB:
-        print('{} already exists'.format(filename))
+    if os.path.exists(path.split('.')[0]):
+        print('Dataset {} already exists at {}'.format(filename.split('.')[0], path.split('.')[0]))
         continue
 
     print('Downloading {}'.format(filename))
