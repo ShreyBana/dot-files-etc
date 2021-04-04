2e9dfea09f9a61e8a80171e0f39912d2f8441ecc
ShreyBana <shreybana26@gmail.com>
2021-04-04 12:22:32 +0530

    updated

--- download_dataset.py
+++ download_dataset.py
@@ -4,25 +4,25 @@ import requests
 import sys
 import os
 
-PATHS = {'DATASET' : ''}
-DOWNLOADED = []
 MB = 1024 * 1024
-URL_LIST = sys.argv[1:]
+downloads_path = '/home/shrey_bana/projects/datasets'
+url_list = sys.argv[1:]
+downloads = []
 
 def print_progress(percentage):
-    print('\r%{:.2f} '.format(percentage) + '[' + '=' * int(percentage * 3 / 10) + ' ' * (30 - int(percentage * 3 / 10)) + ']', end=' ')
+    print('\r%{:.2f} '.format(percentage) + '[' + '=' * (percentage * 3 // 10) + ' ' * (30 - (percentage * 3 // 10)) + ']', end=' ')
 
-for url in URL_LIST:
+for url in url_list:
     print('---------------------------------')
-    filename = url.split('/')[-1].split('.')[0] # assumes downloading w/ .zip ext
-    path = PATHS['DATASET'] + filename
-    PATHS[filename] = path + '/'
+    filename = url.split('/')[-1].split('.')[0] # assumes downloading w/ .zip extension
+    path = os.path.join(downloads_path, filename)
+    downloads.append(filename)
 
     r = requests.get(url, stream=True)
     size = int(r.headers.get('content-length')) / MB
 
     if os.path.exists(path):
-        print('Dataset {} already exists at {}'.format(filename, path))
+        print('Dataset {} already exists @ {}'.format(filename, path))
         continue
 
     print('Downloading {}'.format(filename + '.zip'))
@@ -41,7 +41,7 @@ for url in URL_LIST:
     print('\nUnzipping...')
     
     with ZipFile(path + '.zip', 'r') as myzip:
-        myzip.extractall(PATHS['DATASET'])
+        myzip.extractall(downloads_path)
         myzip.close()
     
     print('Deleting archive...')
@@ -49,8 +49,3 @@ for url in URL_LIST:
     os.remove(path + '.zip')
     
     print('Complete')
-    DOWNLOADED.append(filename)
-
-print('\nFiles downloaded:', end = ' ')
-for filename in DOWNLOADED:
-    print(filename + ' @ {}'.format(PATHS[filename]))
