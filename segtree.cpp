88ae427ef9d40ba88233730140422ae258ecda15
ShreyBana <51321651+ShreyBana@users.noreply.github.com>
2021-04-23 16:40:46 +0530

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class segtree {
+class segtree 
+{
 private:
-  struct node {
-  };
+  struct node {};
   node* tree;
   int size, *arr;
 
-  node merge (const node& lc, const node& rc) {
+  node merge (const node& lc, const node& rc) 
+  {
   }
 
-  void build (int v, int tl, int tr) {
+  void build (int v, int tl, int tr) 
+  {
     if (tl == tr) {
       tree[v] = node (arr[tl]);
     } else {
@@ -22,28 +24,26 @@ private:
     }
   }
 
-  void set (int v, int tl, int tr, ) {
-  }
+  void set (int v, int tl, int tr, ) {}
 
-  type query (int v, int tl , int tr, ) {
-  }
+  type query (int v, int tl , int tr, ) {}
 public:
-  segtree (int *arr, int size):arr(arr), size(size) {
+  segtree (int *arr, int size):arr(arr), size(size) 
+  {
     int log_n = 1;
     while ((1 << log_n) < size) log_n++;
     tree = new node [(1 << (log_n + 1)) - 1];
     build(0, 0, size - 1);
   }
 
-  void set (int ) {
-  }
+  void set (int ) {}
 
-  type query (int ) {
-  }
+  type query (int ) {}
 };
   
 
-int main (void) {
+int main (void) 
+{
   ios_base::sync_with_stdio(false), cin.tie(0);
   int tc; cin >> tc;
   while (tc--) {
