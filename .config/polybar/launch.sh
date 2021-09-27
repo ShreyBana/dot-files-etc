a7604e586f67b6edea84c6b25e1f7eff0e7a2d09
Shrey Bana <shreybana26@gmail.com>
2021-09-27 15:55:13 +0530

    Changed polybar to have rounded corners & moduled some parts

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,9 @@
-mybarusr/bin/env bash
+#!/usr/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
-polybar mybar > ~/.polybar.log
+polybar fullbar 2>&1 | tee -a /tmp/polybar-fullbar.log & disown
+echo "bar launched..."
