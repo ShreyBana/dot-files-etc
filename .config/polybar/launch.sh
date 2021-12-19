0ce8fc217bb905ad206a24a3b36b502adfadde14
Shrey Bana <shreybana26@gmail.com>
2021-12-19 14:46:42 +0530

    Laptop Backup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,5 +5,5 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-polybar fullbar 2>&1 | tee -a /tmp/polybar-fullbar.log & disown
+polybar swift3bar 2>&1 | tee -a /tmp/polybar-swift3bar.log & disown
 echo "Bars launched..."
