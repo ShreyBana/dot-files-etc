24c86ca8eb61bca498ae4fa7485274670283fb2b
Shrey Bana <shreybana26@gmail.com>
2023-05-22 22:32:56 +0530

    Synced with laptop nixos.

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,101 +1,47 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
   home.username = "shrey_bana";
   home.homeDirectory = "/home/shrey_bana";
   programs.git = {
     enable = true;
     userName = "Shrey Bana";
-    userEmail = "shrey.bana@juspay.in";
+    userEmail = "shreybana26@gmail.com";
+    aliases = {
+      co = "checkout";
+      s  = "status";
+    };
   };
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  nixpkgs.overlays = [
-    (self: super: {
-      hls =
-        let 
-          easy-hls-src = super.fetchFromGitHub {
-            owner  = "jkachmar";
-            repo   = "easy-hls-nix";
-            rev    = "6cb50f04e3a61b1ec258c6849df84dae9dfd763f";
-            sha256 = "1rvi6067nw64dka8kksl7f34pwkq7wx7pnhnz3y261fw9z5j4ndp";
-          };
-        in
-          super.callPackage easy-hls-src { ghcVersions = [ "8.8.4" ]; };
-      })
-  ];
   home.packages = with pkgs; [
     htop
     starship
     rofi
-    spotify
     rnix-lsp
-    hls
-    stack
-    nodejs-14_x
-    node2nix
-    haskell.compiler.ghc88
-    android-studio
-    google-chrome-dev
-    rust-analyzer
     direnv
     lorri
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    emacs
+    firefox-devedition-bin
+    feh
+    alacritty
+    spotify
+    tmux
+    exa
+    picom
+    vlc
+    xclip
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  # source
+  # This value determines the Home Manager release that your configuration is
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
   #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/shrey_bana/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -4,14 +4,15 @@ import XMonad.Util.Ungrab
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Loggers
 import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders (noBorders, smartBorders)
+import XMonad.Layout.ToggleLayouts
 import XMonad.Hooks.Script
 import XMonad.Hooks.ManageDocks
-
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.DynamicLog
-import qualified XMonad.DBus as XD
-import qualified DBus.Client as D
+import XMonad.Config.Desktop (desktopLayoutModifiers)
+import XMonad.Hooks.EwmhDesktops(ewmhFullscreen,ewmh)
 import qualified Codec.Binary.UTF8.String as UTF8
-
 
 -- #Colors
 underline = "#abb2bf"
@@ -24,44 +25,42 @@ font = "#abb2bf"
 -- #Main
 main :: IO ()
 main = do 
-    dbus <- XD.connect
-    XD.requestAccess dbus
-    xmonad $ docks $ def
+    xmonad $ ewmhFullscreen $ ewmh $ def
       { terminal = "alacritty"
       , modMask = mod4Mask
-      , layoutHook = spacingRaw False (Border 0 0 0 0) True (Border 2 2 2 2) True $ myLayout
+      , layoutHook = myLayout
       , focusedBorderColor = "#458588"
       , normalBorderColor = "#b16286"
       , borderWidth = 2
       , startupHook = myStartupHook
-      , logHook = dynamicLogWithPP (myLogHook dbus)
+      , manageHook = myManageHook
       }
         `additionalKeysP`
-        [ ("M-S-f" , spawn "firefox")
+        [ ("M-S-f" , spawn "firefox-devedition")
         , ("M-S-p", spawn "rofi -show drun")
         , ("M-<F3>", spawn "brightnessctl s +10%")
         , ("M-<F4>", spawn "brightnessctl s 10%-")
         , ("M-S-s", spawn "flameshot full")
+        , ("M-f", sendMessage $ Toggle "Full")
         ]
 
-
 -- #Log (status bars)
-myLogHook :: D.Client -> PP
-myLogHook dbus = def
-    { ppOutput = XD.send dbus
-    , ppCurrent = wrap ("%{u" ++ underline ++ "}%{+u} ") " %{-u}"
-    , ppUrgent = wrap ("%{u" ++ red ++ "}%{+u} ") " %{-u}"
-    , ppHidden = wrap " " " "
-    , ppWsSep = ""
-    , ppSep = " >>%{F" ++ font ++ "}  "
-    , ppTitle = shorten 25
-    , ppOrder = \(ws:_:t:_) -> [ws,t]
-    }
+--myLogHook :: D.Client -> PP
+--myLogHook dbus = def
+--    { ppOutput = XD.send dbus
+--    , ppCurrent = wrap ("%{u" ++ underline ++ "}%{+u} ") " %{-u}"
+--    , ppUrgent = wrap ("%{u" ++ red ++ "}%{+u} ") " %{-u}"
+--    , ppHidden = wrap " " " "
+--    , ppWsSep = ""
+--    , ppSep = " >>%{F" ++ font ++ "}  "
+--    , ppTitle = shorten 25
+--    , ppOrder = \(ws:_:t:_) -> [ws,t]
+--    }
 
 -- #Layout
-myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
+myLayout = toggleLayouts (noBorders Full) tiled
   where
-    tiled   = Tall nmaster delta ratio
+    tiled   = spacingRaw False (Border 5 5 5 5) True (Border 2 2 2 2) True $ Tall nmaster delta ratio
     nmaster = 001        -- Default number of windows in the master pane
     ratio   = 005 / 008  -- Default proportion of screen occupied by master pane
     delta   = 003 / 100  -- Percent of screen to
@@ -72,6 +71,8 @@ myStartupHook = do
     spawnOnce "picom"
     spawnOnce "alacritty"
     spawnOnce "xsetroot -xcf /usr/share/icons/Adwaita/cursors/left_ptr 32"
-    spawnOnce "xmodmap $HOME/.Xmodmap"
     spawnOnce "exec $HOME/.local/bin/xscreen-lock"
     spawn "$HOME/.config/polybar/./launch.sh"
+
+myManageHook =
+  composeOne [isFullscreen -?> doFullFloat]
