1febb4b8604ed7dd9538b648baa77af69457a0d9
Shrey Bana <shrey.bana@juspay.in>
2023-05-23 10:47:16 +0530

    Synced xmonad & bar.

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -6,11 +6,15 @@ import XMonad.Util.Loggers
 import XMonad.Layout.Spacing
 import XMonad.Hooks.Script
 import XMonad.Hooks.ManageDocks
-
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
-import qualified XMonad.DBus as XD
-import qualified DBus.Client as D
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
 import qualified Codec.Binary.UTF8.String as UTF8
+import qualified Xmobar as Bar
+
+import Xmobar (Runnable(..), Kbd(..), Date(..), XMonadLog(..), Monitors(..))
 
 
 -- #Colors
@@ -23,37 +27,37 @@ font = "#abb2bf"
 
 -- #Main
 main :: IO ()
-main = do 
-    dbus <- XD.connect
-    XD.requestAccess dbus
-    xmonad $ docks $ def
-      { terminal = "alacritty"
-      , modMask = mod4Mask
-      , layoutHook = spacingRaw False (Border 0 0 0 0) True (Border 2 2 2 2) True $ myLayout
-      , focusedBorderColor = "#458588"
-      , normalBorderColor = "#b16286"
-      , borderWidth = 2
-      , startupHook = myStartupHook
-      , logHook = dynamicLogWithPP (myLogHook dbus)
-      }
-        `additionalKeysP`
-        [ ("M-S-f" , spawn "firefox")
-        , ("M-S-p", spawn "rofi -show drun")
-        , ("M-<F3>", spawn "brightnessctl s +10%")
-        , ("M-<F4>", spawn "brightnessctl s 10%-")
-        , ("M-S-s", spawn "flameshot full")
-        ]
+main = xmonad
+  . ewmhFullscreen
+  . ewmh
+  . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+  $ mkConfig
+
+mkConfig = def
+    { terminal = "alacritty"
+    , modMask = mod4Mask
+    , layoutHook = spacingRaw False (Border 2 2 2 2) True (Border 5 5 5 5) True $ myLayout
+    , focusedBorderColor = "#458588"
+    , normalBorderColor = "#b16286"
+    , borderWidth = 3
+    , startupHook = myStartupHook
+    }
+      `additionalKeysP`
+      [ ("M-f" , spawn "firefox")
+      , ("M-S-p", spawn "rofi -show drun")
+      , ("M-<F3>", spawn "brightnessctl s +10%")
+      , ("M-<F4>", spawn "brightnessctl s 10%-")
+      , ("M-S-s", spawn "flameshot full")
+      , ("M-S-x", spawn "xsecurelock")
+      ]
 
 
 -- #Log (status bars)
-myLogHook :: D.Client -> PP
-myLogHook dbus = def
-    { ppOutput = XD.send dbus
-    , ppCurrent = wrap ("%{u" ++ underline ++ "}%{+u} ") " %{-u}"
-    , ppUrgent = wrap ("%{u" ++ red ++ "}%{+u} ") " %{-u}"
-    , ppHidden = wrap " " " "
-    , ppWsSep = ""
-    , ppSep = " >>%{F" ++ font ++ "}  "
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppHidden = wrap " " " "
+    , ppWsSep = " • "
+    , ppSep = " >> "
     , ppTitle = shorten 25
     , ppOrder = \(ws:_:t:_) -> [ws,t]
     }
@@ -68,10 +72,11 @@ myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
 
 -- #Startup
 myStartupHook = do
-    spawnOnce "feh --bg-fill $HOME/pictures/wallpapers/green-leaves.jpg"
-    spawnOnce "picom"
+    spawnOnce "xrandr --output DP-1 --mode 2560x1440 --left-of HDMI-1"
+    spawnOnce "feh --bg-fill $HOME/pictures/wallpapers/pangong-lake-ladakh.jpg"
+    spawnOnce "picom --vsync"
     spawnOnce "alacritty"
     spawnOnce "xsetroot -xcf /usr/share/icons/Adwaita/cursors/left_ptr 32"
-    spawnOnce "xmodmap $HOME/.Xmodmap"
-    spawnOnce "exec $HOME/.local/bin/xscreen-lock"
-    spawn "$HOME/.config/polybar/./launch.sh"
+    --spawnOnce "xmodmap $HOME/.Xmodmap"
+    --spawnOnce "exec $HOME/.local/bin/xscreen-lock"
+    --spawn "/home/shrey_bana/.config/polybar/launch.sh"
